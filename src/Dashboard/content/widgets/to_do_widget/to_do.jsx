5544ad38b57e5e6d6b426fb3cb7eca3a3e52b027
SOUHAIL BEN-LHACHEMI <souhailbenlhachemi1999@gmail.com>
2021-11-20 18:59:21 +0100

    sdfsdf

--- src/Dashboard/content/widgets/to_do_widget/to_do.jsx
+++ src/Dashboard/content/widgets/to_do_widget/to_do.jsx
@@ -8,11 +8,11 @@ const Todo = (props) => {
             {props.tasks.length > 0 ? props.tasks.map((elt)=>(
                 elt.done ? 
                     <div className='row bg-dark mt-3 mr-2 ml-2 rounded' key={elt.todo_id}>
-                    <div className='col-2'><input type="checkbox" onChange={(e)=>{props.update_todos(elt.todo_id)}} checked={elt.done} className='mt-2' style={{marginLeft : '30px',zIndex : '9999'}}/></div>
+                    <div className='col-2'><input type="checkbox" onChange={(e)=>{props.update_todos(elt.todo_id)}} checked={elt.done} className='mt-2' style={{zIndex : '9999'}}/></div>
                     {elt.done ? <div className='col-10'><strike><h6 className='mt-2 text-secondary' >{elt.todo}</h6></strike></div> : <div className='col-10'><h5 className={`mt-2 ${elt.color}`} >{elt.todo}</h5></div>}
                     </div>:
                     <div className='row mt-3 mr-2 ml-2 rounded' style={{background: 'rgba(0,0,0,0.5)'}} key={elt.todo_id}>
-                    <div className='col-2'><input type="checkbox" onChange={(e)=>{props.update_todos(elt.todo_id)}} checked={elt.done} className='mt-2' style={{marginLeft : '30px'}}/></div>
+                    <div className='col-2'><input type="checkbox" onChange={(e)=>{props.update_todos(elt.todo_id)}} checked={elt.done} className='mt-2' style={{zIndex : '9999'}}/></div>
                     {elt.done ? <div className='col-10'><strike><h6 className='mt-2 text-secondary' >{elt.todo}</h6></strike></div> : <div className='col-10'><h5 className={`mt-2 ${elt.color}`} >{elt.todo}</h5></div>}
                     </div>
                 
